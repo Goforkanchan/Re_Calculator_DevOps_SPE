2c78dd22fad5391243ea1810e9f3c840a8f1e2c6
KanchanMahajan <kanchanmahajan67@gmail.com>
2021-03-15 00:22:33 +0530

    Removed explicit exceptions from try, added exception test cases, changed the log formats for grok pattern.

--- src/main/java/iiitb/ac/in/Main.java
+++ src/main/java/iiitb/ac/in/Main.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
@@ -26,33 +27,29 @@ public class Main {
             int choice = Integer.parseInt(br.readLine());
             switch (choice){
                 case 1: {
-                    System.out.println("Enter the number:");
+                    System.out.println("Enter the number for calculating square root:");
                     double num = Double.parseDouble(br.readLine());
                     double result = obj.squareRoot(num);
-                    System.out.println("Square root of the entered number is: " + result);
                     break;
                 }
                 case 2: {
-                    System.out.println("Enter the number:");
+                    System.out.println("Enter the number for calculating factorial:");
                     double fact = Double.parseDouble(br.readLine());
                     double result = obj.factorial(fact);
-                    System.out.println("Factorial of the entered number is: "+ result);
                     break;
                 }
                 case 3:{
-                    System.out.println("Enter the number:");
+                    System.out.println("Enter the number for calculating natural log:");
                     double log = Double.parseDouble(br.readLine());
                     double result = obj.naturalLog(log);
-                    System.out.println("Natural Log of the entered number is: "+ result);
                     break;
                 }
                 case 4: {
-                    System.out.println("Enter the base number:");
+                    System.out.println("Enter the base number for calculating power:");
                     double pow1 = Double.parseDouble(br.readLine());
-                    System.out.println("Enter the exponent:");
+                    System.out.println("Enter the exponent for calculating power:");
                     double pow2 = Double.parseDouble(br.readLine());
                     double result = obj.power(pow1, pow2);
-                    System.out.println("Power of the"+ pow1 +"raised to "+pow2+" is: "+ result);
                     break;
                 }
                 default: {
@@ -65,70 +62,70 @@ public class Main {
 
     }
 
-    private double power(double pow1, double pow2) {
+    public double power(double pow1, double pow2) {
         double result = 0;
         try{
-            logger.info("Base and exponent are :"+ pow1+" ,"+pow2);
+            logger.info("Base and exponent are :["+ pow1+" ,"+pow2+"]");
 
-            if(pow1 <= 0 )
-                throw new ArithmeticException("Base can't be 0 or negative.");
-            else if(pow2 == 0)
+            if(pow2 == 0)
                 result = 1;
             else{
                 result = Math.pow(pow1, pow2);
             }
-        } catch(Exception e){
-            logger.error("Base should be positive integer. ");
+            System.out.println("Power of the"+ pow1 +"raised to "+pow2+" is: "+ result);
+
+        } catch(IllegalArgumentException e){
+            logger.error("Base should be positive integer :["+ pow1+", "+pow2+"]");
         }
         return result;
     }
 
-    private double factorial(double fact) {
+    public double factorial(double fact) {
+        logger.info("Number entered is :["+ fact+"]");
         Double result = 1.0;
         try {
-            if (fact < 0)
-                throw new ArithmeticException("Factorial of negative numbers is not possible!");
-
             while(fact > 0){
                 result *= fact;
                 fact--;
             }
 
-
-        } catch(Exception e){
-            logger.error("Factorial of negative numbers is not possible!"+e.getLocalizedMessage());
-            return result.NaN;
+            System.out.println("Factorial of the entered number is: "+ result);
+        } catch(IllegalArgumentException e){
+            logger.error("Factorial of negative numbers is not possible in catch :["+fact+"]");
         }
         return result;
     }
 
-    private double naturalLog(double log) {
+    public double naturalLog(double log) {
         double result = 0;
         try{
-            logger.info("Base and exponent are :"+ 10+" ,"+log);
+            logger.info("Base and exponent are :["+ 10+" ,"+log+"]");
 
-            if(log < 0 )
-                throw new ArithmeticException("Log of negatives is not defined.");
-            else{
+
                 result = Math.log(log);
-            }
-        } catch(Exception e){
-            logger.error("Log of negatives is not defined.");
+
+            System.out.println("Natural Log of the entered number is: "+ result);
+
+        } catch(IllegalArgumentException e){
+            logger.error("Log of negatives is not defined :["+log+"]");
         }
         return result;
     }
 
-    private double squareRoot(double num) {
+    public double squareRoot(double num) {
         double result = 0;
         try {
-            logger.info("Number for square root:" + num);
-            if (num <= 0) {
-                throw new ArithmeticException("Number is either 0 or less than 0");
-            }
-            result = Math.sqrt(num);
+            logger.info("Number for square root :[" + num+"]");
 
-        } catch (ArithmeticException e) {
-            logger.error("Square root can't be negative or 0 " + e.getLocalizedMessage());
+                //throw new ArithmeticException("Number is either 0 or less than 0");
+
+
+            result = Math.sqrt(num);
+            System.out.println("Square root of the entered number is: " + result);
+
+
+        } catch (IllegalArgumentException e) {
+            logger.error("Square root can't be negative or 0 :["+num+"]");
         }
         return result;
     }
